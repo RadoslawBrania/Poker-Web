e5b1a0d8cd4e1a64cfe445279c40f0160af1e381
Radoslaw Brania <radoslaw02brania@gmail.com>
2024-08-09 10:53:57 +0200

    Dodaj pliki projektów.

---
+++ Poker-Web/Program.cs
@@ -0,0 +1,31 @@
+using Poker_Web.Client.Pages;
+using Poker_Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Poker_Web.Client._Imports).Assembly);
+
+app.Run();
